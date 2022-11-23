f5130de89ce95a3160943daba6863b945dd9b0ab
Ivaylo Ninov <ivailomn216@gmail.com>
2022-11-23 14:21:06 +0200

    MVC pattern - initail commit

---
+++ MoviesInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
